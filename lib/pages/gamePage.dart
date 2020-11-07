48659d77d41424ed8684366aab711591ae53d350
flinsler <frank.linsler@hotmail.de>
2020-11-07 22:51:32 +0100

    First logic in GamePage

--- lib/pages/gamePage.dart
+++ lib/pages/gamePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/topic.dart';
+import '../models/lesson.dart';
 
 import '../provider/sampleTopicProvider.dart';
 
@@ -12,6 +13,32 @@ class GamePage extends StatefulWidget {
 class _GamePageState extends State<GamePage> {
   final Topic _sampleTopic = SampleTopicProvider().getSampleTopics().first;
 
+  int _taskIndex = 0;
+
+  void nextTask() {
+    setState(() {
+      _taskIndex++;
+    });
+  }
+
+  final BoxDecoration answerBoxDecoration = BoxDecoration(
+    color: Colors.green[100],
+    border: Border.all(
+      color: Colors.black,
+      width: 2,
+    ),
+  );
+
+  final TextStyle answerBoxTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 48,
+    color: Colors.purple,
+  );
+
+  final EdgeInsets answerBoxMargin = EdgeInsets.all(10);
+  final double answerBoxWidth = 100;
+  final double answerBoxHeight = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,16 +48,148 @@ class _GamePageState extends State<GamePage> {
         ),
       ),
       body: Center(
-        child: Column(children: [
-          Text('The game'),
-          Text('Topic: ${_sampleTopic.header}'),
-          Text('Lesson: ${_sampleTopic.lessons[0].header}'),
-          Text('Question 1: ${_sampleTopic.lessons[0].tasks[0].question}'),
-          Text('Question 2: ${_sampleTopic.lessons[0].tasks[1].question}'),
-          Text('Question 3: ${_sampleTopic.lessons[0].tasks[2].question}'),
-          Text('Question 4: ${_sampleTopic.lessons[0].tasks[3].question}'),
-          Text('Question 5: ${_sampleTopic.lessons[0].tasks[4].question}'),
-        ]),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('The game'),
+              RaisedButton(
+                onPressed: nextTask,
+                child: Text('Nächste Aufgabe'),
+              ),
+              Table(
+                children: [
+                  TableRow(
+                    children: [
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[0].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[1].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[2].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[3].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[4].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[5].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[6].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[7].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            _sampleTopic
+                                .lessons[0].tasks[_taskIndex].answers[8].text,
+                            style: answerBoxTextStyle,
+                          ),
+                        ),
+                        margin: answerBoxMargin,
+                        decoration: answerBoxDecoration,
+                        width: answerBoxWidth,
+                        height: answerBoxHeight,
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ]),
       ),
     );
   }
